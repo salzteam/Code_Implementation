928efcf3085df3017b9c65af42d5afa5607b485b
Salz <74233105+salzteam@users.noreply.github.com>
2022-10-04 17:00:11 +0700

    Update divideSort.js

--- divideSort.js
+++ divideSort.js
@@ -1,10 +1,8 @@
 function divideAndSort(num) {
-  if (typeof num !== "number") return console.log("Invalid! Must Be Number");
-  console.log(
-    `${num}`
-      .split("0")
-      .map((x) => x.split("").sort().join(""))
-      .join("")
-  );
+  if (typeof num !== "number") return "Invalid! Must Be Number";
+  return `${parseInt(num)}`
+    .split("0")
+    .map((x) => x.split("").sort().join(""))
+    .join("");
 }
-divideAndSort(5956560159466056);
+console.log(divideAndSort(5956560159466056));
